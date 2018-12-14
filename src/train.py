c9e8ba44c67fca3f6d17d1d803c09cb3e6cbe54b
Joshua Shen <38804957+JoshuaSXA@users.noreply.github.com>
2018-12-14 21:41:06 +0800

    add test and loss computation

--- src/train.py
+++ src/train.py
@@ -1,9 +1,14 @@
 import sys
 import os
+import warnings
 import random
+
+
+
 
 import torch
 import numpy as np
+
 
 
 sys.path.append('/Users/shenxiaoang/Git/xLab/src/nn/')
@@ -19,177 +24,124 @@ CLS_HIDDEN_LAYER_DIM_1 = 256
 CLS_HIDDEN_LAYER_DIM_2 = 128
 CLS_OUT_DIM = 1
 TRAIN_EPOCH = 100
-BATCH_SIZE = 10
+BATCH_SIZE = 20
 
-
+warnings.filterwarnings('ignore')
 
 class Trainer(object):
-    def __init__(self, data_path, model_path):
+    def __init__(self, train_data_path, test_data_path, model_path):
         super(Trainer, self).__init__()
-        self._data_path = data_path
+        self._train_data_path = train_data_path
+        self._test_data_path = test_data_path
         self._model_path = model_path
-        self._file_set = None
-        self._file_tag = None
+        self._train_file_list = None
+        self._test_file_list = None
+        self._train_file_tag = None
+        self._test_file_tag = None
         self._data_analysis = PathAnalysis(self._model_path)
         self._tree_lstm = TreeLSTM(EMBED_DIM, EMBED_DIM, CLS_HIDDEN_LAYER_DIM_1, CLS_HIDDEN_LAYER_DIM_2, CLS_OUT_DIM)
 
-    def load_data(self, file_type):
-        print('Loading data from %s' % (self._data_path))
-        for root, dirs, files in os.walk(self._data_path):
+    def load_data(self, file_type, train=True):
+        if train:
+            data_path = self._train_data_path
+        else:
+            data_path = self._test_data_path
+        print('Loading data from %s' % (data_path))
+        for root, dirs, files in os.walk(data_path):
             all_files = files
-        self._file_set = []
+        file_list = []
         # 抽取出相应类型的文件
         for i in range(len(all_files)):
             if file_type in all_files[i]:
-                self._file_set.append(all_files[i])
+                file_list.append(all_files[i])
         # 加载文件标签
-        self._file_tag = np.array([])
+        file_tag = np.array([])
         # 统计正负样本的数量
         negSamp = 0
         posSamp = 0
-        for i in range(0, len(self._file_set)):
-            tag = int(self._file_set[i][-5])
+        for i in range(0, len(file_list)):
+            tag = int(file_list[i][-5])
             if tag == 0:
                 posSamp += 1
             else:
                 negSamp += 1
-            self._file_tag = np.append(self._file_tag, tag)
-        self._file_tag = torch.from_numpy(self._file_tag)
-        self._file_tag = self._file_tag.view(list(self._file_tag.size())[0], 1)
-        print('Loaded %d %s files from %s, including %d positive samples and %d negative samples.' % (len(self._file_set), file_type, self._data_path, posSamp, negSamp))
+            file_tag = np.append(file_tag, tag)
+        file_tag = torch.from_numpy(file_tag)
+        file_tag = file_tag.view(list(file_tag.size())[0], 1)
+        print('Loaded %d %s files from %s, including %d positive samples and %d negative samples.' % (len(file_list), file_type, data_path, posSamp, negSamp))
+        if train:
+            self._train_file_list = file_list
+            self._train_file_tag = file_tag
+        else:
+            self._test_file_list = file_list
+            self._test_file_tag = file_tag
 
 
     def data_shuffle(self):
-
-        self._file_tag = self._file_tag.view(list(self._file_tag.size())[0]).numpy().tolist()
-
-        print(self._file_tag)
-
+        self._train_file_tag = self._train_file_tag.view(list(self._train_file_tag.size())[0]).numpy().tolist()
+        print(self._train_file_tag)
         randnum = random.randint(0, 100)
         random.seed(randnum)
-        random.shuffle(self._file_set)
+        random.shuffle(self._train_file_list)
         random.seed(randnum)
-        random.shuffle(self._file_tag)
-        self._file_tag = torch.from_numpy(np.array(self._file_tag))
-        self._file_tag = self._file_tag.view(list(self._file_tag.size())[0], 1)
-
+        random.shuffle(self._train_file_tag)
+        self._train_file_tag = torch.from_numpy(np.array(self._train_file_tag))
+        self._train_file_tag = self._train_file_tag.view(list(self._train_file_tag.size())[0], 1)
 
 
     def train(self):
         criterion = torch.nn.BCELoss()
         optimizer = torch.optim.Adam(self._tree_lstm.parameters(), lr=0.01)
+        optimizer.zero_grad()
+        total_loss = 0.0
         self.data_shuffle()
-        for i in range(len(self._file_set)):
-            print(self._file_set[i], i)
-            self._data_analysis.predict_file(os.path.join(self._data_path, self._file_set[i]))
+        for i in range(len(self._train_file_list)):
+            self._data_analysis.predict_file(os.path.join(self._train_data_path, self._train_file_list[i]))
             _tree_dict = self._data_analysis.get_hash_tree()
             _tree_vec = self._data_analysis.node_vec_dict
             _tree_root = self._data_analysis.tree_root
             output = self._tree_lstm(_tree_dict, _tree_vec, _tree_root)
-            print(output)
-            optimizer.zero_grad()
-            loss = criterion(output[0], self._file_tag[i].float())
+            loss = criterion(output[0], self._train_file_tag[i].float())
+            total_loss += loss.item()
             loss.backward()
-            optimizer.step()
-            print(loss)
+            if (i + 1) % BATCH_SIZE == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+                print(total_loss)
+        return total_loss / len(self._train_file_list)
+
+    def test(self):
+        self._tree_lstm.eval()
+        criterion = torch.nn.BCELoss()
+        with torch.no_grad():
+            total_loss = 0.0
+            predictions = torch.FloatTensor(torch.zeros(len(self._test_file_list), 1))
+            for i in range(len(self._test_file_list)):
+                self._data_analysis.predict_file(os.path.join(self._test_data_path, self._test_file_list[i]))
+                _tree_dict = self._data_analysis.get_hash_tree()
+                _tree_vec = self._data_analysis.node_vec_dict
+                _tree_root = self._data_analysis.tree_root
+                output = self._tree_lstm(_tree_dict, _tree_vec, _tree_root)
+                #print(output, self._test_file_tag[i])
+                predictions[i] = output.ge(0.5).float()
+                loss = criterion(output[0], self._test_file_tag[i].float())
+                total_loss += loss.item()
+            accuracy = 1 - (predictions - self._test_file_tag.float()).abs_().sum() / len(self._test_file_tag)
+        return total_loss / len(self._test_file_list), accuracy
 
 
-
-
-
-# class Train(object):
-#     def __init__(self, data_path, model_path):
-#         self._data_path = data_path
-#         self._model_path = model_path
-#         self._data_analysis = PathAnalysis(self._model_path)
-#         self._lstm_cell = TreeLSTMCell(TENSOR_DIM, HIDDEN_LAYER_DIM_1, HIDDEN_LAYER_DIM_2, tf.random_normal_initializer(mean=0, stddev=1))
-#         self._file_set = None
-#         self._file_tag = None
-#         self._tree_root = None
-#         self._hash_tree = None
-#         self._node_vec_dict = None
-#         self._train_index = 0
-#         self._train_epoch = 1
-#
-#     def clear(self):
-#         self._tree_root = None
-#         self._hash_tree = None
-#         self._node_vec_dict = None
-#
-#     def load_data(self, file_type):
-#         print('Loading data from %s' % (self._data_path))
-#         for root, dirs, files in os.walk(self._data_path):
-#             all_files = files
-#         self._file_set = np.array([])
-#         # 抽取出相应类型的文件
-#         for i in range(0, len(all_files)):
-#             if file_type in all_files[i]:
-#                 self._file_set = np.append(self._file_set, all_files[i])
-#         # 加载文件标签
-#         self._file_tag = np.array([])
-#         # 统计正负样本的数量
-#         negSamp = 0
-#         posSamp = 0
-#         for i in range(0, np.size(self._file_set)):
-#             tag = int(self._file_set[i][0])
-#             if tag == 0:
-#                 posSamp += 1
-#             else:
-#                 negSamp += 1
-#             self._file_tag = np.append(self._file_tag, tag)
-#         print('Loaded %d %s files from %s, including %d positive samples and %d negative samples.' % (len(self._file_set), file_type, self._data_path, posSamp, negSamp))
-#
-#     def tree_cursor(self, node_index):
-#         leaf_node = True
-#         # 获取当前节点的特征向量，如果dict中不存在的话就直接返回零向量
-#         try:
-#             feature_tensor = self._node_vec_dict[node_index]
-#         except:
-#             return np.array([tf.Variable(tf.zeros((TENSOR_DIM, 1))), tf.Variable(tf.zeros((TENSOR_DIM, 1)))])
-#         # 获取当前节点的子节点
-#         children_node = self._hash_tree[node_index]
-#         # 子节点输出的h\c值组成的np array
-#         hc_input = None
-#         for i in range(0, len(children_node)):
-#             tmp_hc = np.array([self.tree_cursor(children_node[i])])
-#             if i == 0:
-#                 leaf_node = False
-#                 hc_input = tmp_hc
-#             else:
-#                 hc_input = np.concatenate([hc_input, tmp_hc])
-#         # 如果是叶节点的话直接返回零向量
-#         if leaf_node:
-#             hc_input = np.array([[tf.Variable(tf.zeros((TENSOR_DIM, 1))), tf.Variable(tf.zeros((TENSOR_DIM, 1)))]])
-#         self._lstm_cell.init_inputs(hc_input, feature_tensor)
-#         return self._lstm_cell.lstm_cell()
-#
-#     # 每次迭代都要调用一次该函数
-#     def __call__(self):
-#         if self._train_index >= len(self._file_set):
-#             self._train_index = 0
-#             self._train_epoch += 1
-#         # 重置参数
-#         self.clear()
-#         # 数据预处理
-#         self._data_analysis.predict_file(os.path.join(self._data_path, self._file_set[self._train_index]))
-#         self._hash_tree = self._data_analysis.get_hash_tree()
-#         self._node_vec_dict = self._data_analysis.node_vec_dict
-#         self._tree_root = self._data_analysis.tree_root
-#         # 构建训练图
-#         tree_lstm_output = self.tree_cursor(self._tree_root)
-#         out_put = self._lstm_cell.classifier(tree_lstm_output[0])
-#         label = tf.constant(self._file_tag[self._train_index], dtype=tf.float32, shape=(1, 1))
-#         return out_put, label
-#
-#     def get_train_batches(self):
-#         return len(self._file_set)
-#
 if __name__ == "__main__":
-    data_path = os.path.join('..', 'data', 'code_snippet', 'dataset')
+    train_data_path = os.path.join('..', 'data', 'code_snippet', 'Dataset2018', 'train_data')
+    test_data_path = os.path.join('..', 'data', 'code_snippet', 'Dataset2018', 'test_data')
     model_path = os.path.join('..', 'data', 'models', 'java14_model', 'saved_model_iter8.release')
-    train = Trainer(data_path, model_path)
+    train = Trainer(train_data_path, test_data_path, model_path)
     train.load_data('.txt')
-    train.train()
+    train.load_data('.txt', False)
+    for i in range(TRAIN_EPOCH):
+        train_loss = train.train()
+        test_loss, accuracy = train.test()
+        print("EPOCH %d: train loss is %f, test loss is %f, test accuracy is %f" % ((i + 1), train_loss, test_loss, accuracy))
+        torch.save(train._tree_lstm.state_dict(), os.path.join('..', 'data', 'models', 'tree_lstm_model', 'model_epoch_%d.pkl' % (i + 1)))
 
 
 
